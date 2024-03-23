fbe0c9a7e91c8d110b02a5c487fa9ad2f2d5d2bc
revolutioncomputers <revolutioncomputerstech@gmail.com>
2024-03-23 00:32:22 -0400

    fuck off

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axio = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -33,13 +34,24 @@ public_users.post("/register", (req,res) => {
   }
 });
 
+let bookListPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve("Promise resolved")
+    },6000)})
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(300).json({Books: books});
+    Promise.all([bookListPromise])
+        .then(([successMessage]) => {
+            console.log("From Callback " + successMessage);
+            return res.status(200).json({ Books: books });
+        })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+    Promise.all([bookListPromise])
+        .then(([successMessage]) => {
+            console.log("From Callback " + successMessage);
     const isbn = req.params.isbn;
     const booksArray = Object.values(books);
     const filteredBooks = booksArray.filter(book => book.isbn === isbn);
@@ -50,10 +62,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
     else{
         res.status(300).json({Books: "No Matches for " + isbn + " found"});
     }
+})
     });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+    Promise.all([bookListPromise])
+    .then(([successMessage]) => {
+        console.log("From Callback " + successMessage);
 const author = req.params.author;
 const booksArray = Object.values(books);
 const filteredBooks = booksArray.filter(book => book.author === author);
@@ -64,10 +80,14 @@ return res.status(300).json({Books: filteredBooks});
 else{
     res.status(300).json({Books: "No Matches for " + author + " found"});
 }
+    })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
+    Promise.all([bookListPromise])
+    .then(([successMessage]) => {
+        console.log("From Callback " + successMessage);
     const title = req.params.title;
     const booksArray = Object.values(books);
     const filteredBooks = booksArray.filter(book => book.title === title);
@@ -78,6 +98,7 @@ public_users.get('/title/:title',function (req, res) {
     else{
         res.status(300).json({Books: "No Matches for " + title + " found"});
     }
+})
 });
 
 //  Get book review
@@ -88,7 +109,7 @@ public_users.get('/review/:isbn',function (req, res) {
     if (filteredBooks!= "")
     {
     const reviews = filteredBooks.map(book => book.reviews);
-    return res.status(300).json({Books: JSON.stringify(reviews)});
+    return res.status(300).json({reviews: JSON.stringify(reviews)});
     }
     else{
         res.status(300).json({Books: "No Matches for " + isbn + " found"});
